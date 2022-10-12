206da4d58e5247587622f3dc3cc94c7f6903673f
Imran940 <immoshaikh91@gmail.com>
2022-10-13 01:28:59 +0530

    added react-icons and refactored header code

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,42 +1,71 @@
 import React, { useState } from "react";
-import MenuItems from "./MenuItems";
+import { Link } from "react-router-dom";
+import { GrProjects } from "react-icons/gr";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { FiSun } from "react-icons/fi";
+import { AiOutlineMenuFold } from "react-icons/ai";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
 
 const Header = () => {
   const [active, setActive] = useState(false);
-  /*
-  const showMenu = () => {
-    setActive(!active);
-  };*/
 
-  //   window.addEventListener("resize", () => {
-  //     setActive(false);
-  //   });
+  const Links = (
+    <>
+      <Link className="liItem" to="#home">
+        Home
+      </Link>
+      <Link className="liItem" to="#projects">
+        Projects
+      </Link>
+      <Link className="liItem" to="#skills">
+        Skills
+      </Link>
+      <Link className="liItem" to="#aboutUs">
+        About Us
+      </Link>
+    </>
+  );
+  const MenuItems = (
+    <div className="fixed left-0 top-0 w-1/2 h-full  bg-black/10 z-10 sm:hidden">
+      <ul
+        className={
+          "text-white flex-col flex items-center p-7 gap-7 w-full h-full mt-10"
+        }
+      >
+        {Links}
+        <li>
+          <FiSun size={25} />
+        </li>
+      </ul>
+    </div>
+  );
 
   return (
     <div className="sticky top-0 w-full text-white font-normal text-[20px] p-4 flex justify-between">
-      <div className="font-mono  items-center gap-2 flex">
-        <i className="fa-brands fa-slack"></i>
+      <div className="font-mono cursor-pointer items-center gap-2 flex">
+        <GrProjects className="bg-white" />
         <span>Portfolio</span>
       </div>
 
       <ul className="hidden text-center font-sans justify-between w-[60%] sm:flex">
-        <li>Home</li>
-        <li>Projects</li>
-        <li>Skills</li>
-        <li>About Us</li>
+        {Links}
       </ul>
 
-      {active && <MenuItems />}
-      <i
-        onClick={() => setActive(!active)}
-        className={`fa-solid cursor-pointer sm:hidden ${
-          active ? "fa-xmark" : "fa-bars"
-        }`}
-      ></i>
-
+      {active && MenuItems}
+      {active ? (
+        <AiOutlineMenuFold
+          className="sm:hidden"
+          onClick={() => setActive(!active)}
+        />
+      ) : (
+        <AiOutlineMenuUnfold
+          className="sm:hidden"
+          onClick={() => setActive(!active)}
+        />
+      )}
       <div className="hidden w-[5%] justify-between items-center sm:flex ">
-        <i class="fa-solid fa-user"></i>
-        <i class="fa-solid fa-sun px-3"></i>
+        <MdOutlineDarkMode size={25} />
+        <FiSun size={25} />
       </div>
     </div>
   );
